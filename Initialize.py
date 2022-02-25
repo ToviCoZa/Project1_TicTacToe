217be419094f96730c386b650fd627013fae4860
Vito <vito.coquet@gmail.com>
2022-02-25 23:57:17 +0100

    corrected typos in Initialize

--- Initialize.py
+++ Initialize.py
@@ -1,12 +1,10 @@
 import random
-player_1 = input("Please insert the player name: " )
-player_2 = input("plese insert an another player name: " )
+player_1 = input("Player 1 please enter your name : ")
+player_2 = input("Player 2 please enter your name : ")
 
-def initialization () :
-    if random.randint(0,100) <=50: 
-        starting_player = player_1 
-    else:
-        starting_player = player_2
+def initialization() :
+    if random.randint(0,100) <= 50 : starting_player = player_1 
+    else: starting_player = player_2
     print(starting_player)
 
 initialization()
