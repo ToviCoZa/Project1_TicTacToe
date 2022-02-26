5df97cf7e5a1c45b2ada95d1f26112ea54d2a4f8
Vito Coquet <vito.coquet@gmail.com>
2022-02-26 23:24:16 +0100

    reworking from scratch because it got too complex

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,76 +1,60 @@
 import random
-    
-p1_aligned_marks = 0
-p2_aligned_marks = 0
 
+player_1 = ({'Name' : input("Player 1 please enter your name : ")})
+player_2 = ({'Name' : input("Player 2 please enter your name : ")})
 
-
-player_1 = dict()
-player_2 = dict()
-player_1.update({'Name' : input("Player 1 please enter your name : ")})
-player_2.update({'Name' : input("Player 1 please enter your name : ")})
-player_1.update({'Symbol' : 'X'})
-player_2.update({'Symbol' : 'O'})
-player_1.update({'cellsPlayed' : [1, 2 , 3 , 4, 5, 6, 7, 8, 9]}) 
-player_2.update({'cellsPlayed' : [1, 2 , 3 , 4, 5, 6, 7, 8, 9]}) 
-player_1.update({'aligned_marks' : 0})
-player_2.update({'aligned_marks' : 0})
-
-board = {}
+board = [-1] * 9
 
 def whoIsGoingToStart():
-    if random.randint(0,100) <= 50 : 
-        starting_player = player_1
-    else : 
-        starting_player = player_2
-    return(starting_player)
+  if random.randint(0,100) <= 50 : 
+      starting_player = player_1
+  else : 
+      starting_player = player_2
+  return(starting_player)
 
-def create_board() :
-    i = 1
-    for i in range(10) :
-        board.update({i: False})
-    print(board)
+def draw_board() :
+  for y in range(3):
+    for x in range(3):
+      if board[x + 3 * y] == -1: print(str(x + 3 * y) + "  ", end="")
+      elif board[x + 3 * y] % 2 == 0: print("X  ", end="")
+      else: print("O  ", end="")
+    print()
+  pass
 
+def takeTurns():
+  countOfTurns = 0
+  if whoIsGoingToStart() is player_2:
+    countOfTurns = 1 
+  while True:
+    draw_board()
+    selected_cell = input("Please input the number of the cell you want to play in : ")
+    if selected_cell.isdigit and len(selected_cell) == 1 and board[int(selected_cell)] == -1: #Si on est sur une case libre, on avance d'un tours
+      board[int(selected_cell)] = countOfTurns;
+      countOfTurns += 1
+      if checkWinCondition() : break #Si quelqu'un à gagné
+      if fullBoard() : #Si la grille est pleine
+        print("It's a draw !")
+        break
+  draw_board()
+  
 
-
-def takeTurns(player):
-
-    countOfTurns = 0
-    hasGameEnded = False
-    
-    while countOfTurns < 9 and hasGameEnded == False:
-        if countOfTurns % 2 == 0 :
-            player = player_1
-        else :
-            player = player_2
-
-        selected_cell = input("Please input the number of the cell you want to play in : ")
-        if selected_cell.isdigit and len(selected_cell) == 1:
-            for i in player.get('cellsPlayed') :
-                if str(i) == selected_cell :
-                    
-                    player.get('cellsPlayed').remove(i)
-                    print(player.get('cellsPlayed'))
-                    cellToFill = board.get(selected_cell)
-                    if cellToFill == False : board.update({str(selected_cell):player.get('Symbol')})
-                    if checkWinCondition(player) == 'Win':
-                        print("You win ! ")
-                        hasGameEnded = True
-                    countOfTurns += 1
-                            
-        else : pass
-    print("It's a draw !")
-
-def checkWinCondition(p):
-    solutions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-    for i in solutions :
-        for j in i :
-            if board.get(j) == p.get('Symbol'):
-                print(board.get(j))
-                p.update('aligned_marks': player.get('aligned_marks'+ 1
-                print(p.get(aligned_marks))
-                if player.aligned_marks == 3 :
-                    return 'win'
+def fullBoard():
+  for i in range(9):
+    if board[i] == -1: return False
+  return True
 
 
-takeTurns(whoIsGoingToStart())
+def checkWinCondition():
+  solutions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  for i in solutions :
+    if board[i[0]] != -1 and board[i[1]] != -1 and board[i[2]] != -1: #Si on test 3 cases remplies 
+      if board[i[0]] % 2 == board[i[1]] % 2 == board[i[2]] % 2: #Si les 3 cases ont été placées par le meme joueur
+        if i[0] % 2 == 0: #win du joueur 1
+          print(player_1.get('Name') + " has won !")
+        else: #win du joueur 2
+          print(player_2.get('Name') + " has won !")
+        return True
+  return False
+
+
+takeTurns()
