558937b3464cd318e4a0ac92ee6a92df863b37f4
Jamie Sims <jamie14676@gmail.com>
2022-02-27 17:41:36 +0100

    WinValidation

--- WinValidation.py
+++ WinValidation.py
@@ -1,5 +1,5 @@
 def check_win(player_num, current_player):
-    pattern = [[0, 1, 2][3, 4, 5][6, 8, 7][0, 3, 6][1, 4, 7][2, 5, 8][0, 4, 8][6, 4, 2]]
+    pattern = [[0, 1, 2],[3, 4, 5],[6, 8, 7],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[6, 4, 2]]
     for x in pattern:
         if all(y in player_num[current_player] for y in x):
             return True  
